f9d19de6c58847f460834e59c71d6145b22bec84
guohaozhao <babyduncan@qq.com>
2013-11-15 22:24:06 +0800

    init project

---
+++ src/main/java/com/babyduncan/getq/App.java
@@ -0,0 +1,13 @@
+package com.babyduncan.getq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
