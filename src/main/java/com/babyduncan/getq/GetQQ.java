dded228284a1664dbe961a85ea681db203b5d7bb
guohaozhao <babyduncan@qq.com>
2013-11-18 12:19:30 +0800

    change qq

--- src/main/java/com/babyduncan/getq/GetQQ.java
+++ src/main/java/com/babyduncan/getq/GetQQ.java
@@ -48,7 +48,7 @@ public class GetQQ {
         try {
 
             String[] targets = {
-                    "/profiles/737191831"};
+                    "/profiles/1916960731"};
 
             List<String> qqList = new ArrayList<String>();
 
